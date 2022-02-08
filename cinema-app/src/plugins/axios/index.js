178ff27e64535d575ab3709c407db4b061d423b7
Roman <darkkisblack1994@yandex.ru>
2022-02-08 19:02:46 +0700

    настроил стору

--- cinema-app/src/plugins/axios/index.js
+++ cinema-app/src/plugins/axios/index.js
@@ -1,4 +1,10 @@
 import axios from "axios";
+import interceptor from "./interceptor";
 
-const instance = axios.create({});
+const instance = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+});
+
+interceptor(instance);
+
 export default instance;
--- cinema-app/src/store/index.js
+++ cinema-app/src/store/index.js
@@ -1,11 +1,12 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import moviesStore from "./modules/movies";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
-  actions: {},
+  actions: { moviesStore },
   modules: {},
 });
