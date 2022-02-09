db077a606fce9fd1e69b7ad98be54bac6443901a
Roman <darkkisblack1994@yandex.ru>
2022-02-09 13:45:18 +0700

    added poster on background

--- cinema-app/src/App.vue
+++ cinema-app/src/App.vue
@@ -1,24 +1,34 @@
 <template>
   <div id="app">
-    <MoviesList :list="moviesList" />
+    <PosterBg :poster="posterBg" />
+    <MoviesList :list="moviesList" @changePoster="onChangePoster" />
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 import MoviesList from "@/components/MoviesList";
+import PosterBg from "@/components/PosterBg";
 
 export default {
   name: "app",
   components: {
     MoviesList,
+    PosterBg,
   },
+
+  data: () => ({
+    posterBg: "",
+  }),
   computed: {
     ...mapGetters("movies", ["moviesList"]),
   },
 
   methods: {
     ...mapActions("movies", ["fetchMovies"]),
+    onChangePoster(poster) {
+      this.posterBg = poster;
+    },
   },
 };
 </script>
@@ -28,5 +38,6 @@ export default {
   font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  position: relative;
 }
 </style>
